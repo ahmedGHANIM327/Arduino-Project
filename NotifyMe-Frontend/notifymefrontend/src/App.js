f0ef6abae80ed1d2d0395f44d26702df03e3348e
ahmedGHANIM327 <ahmedghanim902@gmail.com>
2022-01-04 18:41:03 +0100

    and also App.js

--- NotifyMe-Frontend/notifymefrontend/src/App.js
+++ NotifyMe-Frontend/notifymefrontend/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Sidenav from './Sidenav/Sidenav';
+import { BrowserRouter as Router , Route, Routes , NavLink } from 'react-router-dom';
+import Home from './HOME/Home';
+import Dashboard from './Dashboard/Dashboard'
+import Employees from './Employees/Employees'
+import Messages from './Messages/Messages'
+import Groups from './Groups/Groups'
 
 function App() {
   return (
     <div className="App">
-      <Sidenav></Sidenav>
+      <Router>
+            <div >
+                <Routes>
+                    <Route path="/" exact={true} element={<Home />} />
+                    <Route path="/notifyme/" exact={true} element={<Sidenav />} >
+                      <Route path="/notifyme/" exact={true} element={<Dashboard />} />
+                      <Route path="/notifyme/employees" exact={true} element={<Employees />} />
+                      <Route path="/notifyme/messages" exact={true} element={<Messages />} />
+                      <Route path="/notifyme/groups" exact={true} element={<Groups />} />
+                    </Route>
+                </Routes>
+            </div>
+        </Router>
     </div>
   );
 }
--- NotifyMe-Frontend/notifymefrontend/src/index.js
+++ NotifyMe-Frontend/notifymefrontend/src/index.js
@@ -11,19 +11,7 @@ import Groups from './Groups/Groups'
 
 ReactDOM.render(
   <React.StrictMode>
-        <Router>
-            <div >
-                <Routes>
-                    <Route path="/" exact={true} element={<Home />} />
-                    <Route path="/notifyme/" exact={true} element={<App />} >
-                      <Route path="/notifyme/" exact={true} element={<Dashboard />} />
-                      <Route path="/notifyme/employees" exact={true} element={<Employees />} />
-                      <Route path="/notifyme/messages" exact={true} element={<Messages />} />
-                      <Route path="/notifyme/groups" exact={true} element={<Groups />} />
-                    </Route>
-                </Routes>
-            </div>
-        </Router>
+        <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
